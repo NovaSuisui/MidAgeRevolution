ccad6fd47feb3c96aeebc741dba8414cae55a42a
NovaSuisui <99822346+NovaSuisui@users.noreply.github.com>
2022-05-13 19:31:42 +0700

    test

--- AllSprite/Obstacle.cs
+++ AllSprite/Obstacle.cs
@@ -40,7 +40,7 @@ namespace MidAgeRevolution.AllSprite
 
         public override void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(test, new Vector2(position.X, position.Y), null, Color.Black, 0, origin, new Vector2(scale.X, scale.Y), SpriteEffects.None, 0f);
+            spriteBatch.Draw(test, new Vector2(position.X, position.Y), null, Color.Black, 0, origin, scale, SpriteEffects.None, 0f);
             base.Draw(spriteBatch);
         }
     }
